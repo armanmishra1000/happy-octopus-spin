eaf28df6c449ffca2e6ba385933b365284bcadf9
[dyad] <armanmishra1111@gmail.com>
2025-10-07 15:29:36 +0530

    [dyad] Allowing external images in Next.js config - wrote 1 file(s)

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,14 @@ const nextConfig: NextConfig = {
     }
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
